b8fc898c9286d6d2f3a17bcb51c22ba6e9f6f84e
mikael <mustafamikael.bektas@ynov.com>
2026-02-24 15:48:28 +0100

    gkm,mkng,

--- sql/quete1.sql
+++ sql/quete1.sql
@@ -1,16 +1,18 @@
 -- Quête 1 : Découverte du SQL
 -- 1. Afficher tous les véhicules
 -- Cette requête permet de lister tous les véhicules présents dans la base.
-SELECT * FROM vehicule;
+SELECT * FROM vehicules;
 
 -- 2. Afficher uniquement les véhicules disponibles
 -- On filtre sur la colonne etat pour ne garder que les véhicules disponibles.
-SELECT * FROM vehicule WHERE etat = 'disponible';
+SELECT * FROM vehicules WHERE etat = 'Disponible';
 
 -- 3. Afficher les véhicules situés dans une ville donnée
 -- On filtre sur la colonne ville. Remplacez 'Paris' par la ville souhaitée.
-SELECT * FROM vehicule WHERE ville = 'Paris';
+SELECT *
+FROM vehicules
+WHERE localisation = 'Paris';
 
 -- 4. Afficher les véhicules ayant une autonomie supérieure à 400 km
 -- On filtre sur la colonne autonomie_km.
-SELECT * FROM vehicule WHERE autonomie_km > 400;
+SELECT * FROM vehicules WHERE autonomie_km > 400;
